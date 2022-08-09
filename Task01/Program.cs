cbd7c6ef8dd2fc241aff37543c6749c53a771121
yana sarvarova <serg.yana@mail.ru>
2022-08-09 10:38:29 +0300

    Задаем элементы массива с клавиатуры

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Программа, которая из имеющегося массива строк формирует массив из строк, длина
+которых меньше либо равна 3 символа. Первоначальный массив вводится с клавиатуры */
+
+Console.WriteLine("Введите массив 1");
+string elementOne = Console.ReadLine();
+Console.WriteLine("Введите массив 2");
+string elementTwo = Console.ReadLine();
+Console.WriteLine("Введите массив 3");
+string elementThree = Console.ReadLine();
+Console.WriteLine("Вывод массива из строк, длина которых меньше или равна 3 символа:");
